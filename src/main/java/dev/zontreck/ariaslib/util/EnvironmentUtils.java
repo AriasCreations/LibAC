eb5fc74e08c4985292681c7402bcb3c4f6ea9c19
Aria <tarapiccari@gmail.com>
2023-06-22 15:29:54 -0700

    Add fallback method for checking docker.

--- src/main/java/dev/zontreck/ariaslib/util/EnvironmentUtils.java
+++ src/main/java/dev/zontreck/ariaslib/util/EnvironmentUtils.java
@@ -5,15 +5,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class EnvironmentUtils
-{
-	public static boolean isRunningInsideDocker()
-	{
-		try{
-			Stream<String>  str = Files.lines ( Paths.get ( "/proc/1/cgroup" ) );
-			return str.anyMatch ( ln->ln.contains ( "/docker" ) );
-		}catch( IOException e )
-		{
+public class EnvironmentUtils {
+	public static boolean isRunningInsideDocker ( ) {
+		if ( Files.exists ( Paths.get ( "/.dockerenv" ) ) )
+			return true;
+		try {
+			Stream<String> str = Files.lines ( Paths.get ( "/proc/1/cgroup" ) );
+			return str.anyMatch ( ln -> ln.contains ( "/docker" ) );
+		} catch ( IOException e ) {
 			return false;
 		}
 	}
