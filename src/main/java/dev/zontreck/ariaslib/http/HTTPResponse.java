615049ab7b14a9bcc2e7e3c2069674698ae650d2
zontreck <tarapiccari@gmail.com>
2023-12-05 13:26:31 -0700

    Fixes null response values

--- src/main/java/dev/zontreck/ariaslib/http/HTTPResponse.java
+++ src/main/java/dev/zontreck/ariaslib/http/HTTPResponse.java
@@ -5,8 +5,13 @@ public class HTTPResponse
     private String ContentType;
     private int ResponseCode;
     private String ResponseBody;
+    private HTTPRequest OriginalRequest;
 
     protected HTTPResponse(String contentType, int code, String body, HTTPRequest request){
+        this.ContentType = contentType;
+        this.ResponseCode = code;
+        this.ResponseBody = body;
+        this.OriginalRequest = request;
 
     }
 
@@ -20,5 +25,9 @@ public class HTTPResponse
 
     public String getResponseBody() {
         return ResponseBody;
+    }
+
+    public HTTPRequest getOriginalRequest() {
+        return OriginalRequest;
     }
 }
